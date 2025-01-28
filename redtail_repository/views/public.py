55700a7caac2bc17eaba2ec26102eb68ff24223e
Pablo Orduña <pablo@labsland.com>
2025-01-28 11:31:40 -0800

    Small changes in public and lessons

--- redtail_repository/views/public.py
+++ redtail_repository/views/public.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
+
+from redtail_repository import db
+from redtail_repository.models import Lesson
 
 public_blueprint = Blueprint('public', __name__)
 
@@ -10,7 +13,83 @@ def index():
 @public_blueprint.route('/lessons')
 def lessons():
 # TODO: Add calls to database to populate page
-    return render_template('public/lessons.html')
+
+    all_categories = [
+        { 
+            "name": "Digital Twin",
+            "slug": "digital-twin",
+            "id": 1,
+        },
+        {
+            "name": "Simulation",
+            "slug": "simulation",
+            "id": 2,
+        },
+        {
+            "name": "Real-world",
+            "slug": "real-world",
+            "id": 3,
+        }
+    ]
+
+    all_categories_by_slug = { category['slug']: category for category in all_categories}
+
+    lessons = [
+        {
+            "name": "Parking Lot",
+            "categories": [
+                1,
+                2
+            ]
+        },
+        {
+            "name": "NES Controller",
+            "categories": [
+                1,
+            ]
+        }, 
+        {
+            "name": "Elevator",
+            "categories": [
+                3,
+            ]
+        }, 
+        {
+            "name": "Keypad",
+            "categories": [
+                2,
+            ]
+        }
+    ]
+
+    category = request.args.get('category')
+    if category:
+        # the user has requested a category
+        category_id = (all_categories_by_slug.get(category) or {}).get('id')
+        if category_id:
+            lessons = [ lesson for lesson in lessons if category_id in lesson.get('categories', [])]
+
+    
+    # lessons = [ 
+    #     {
+    #         'name': lesson.name,
+    #         'description': lesson.description,
+    #     }
+    #     for lesson in db.session.query(Lesson).filter_by(active=True).all()
+    # ]
+    # 
+    return render_template('public/lessons.html', lessons=lessons, all_categories=all_categories)
+
+
+@public_blueprint.route('/lessons/<lesson_slug>')
+def lesson(lesson_slug):
+    # human-friendly-slug
+    # parking-log-{12sdf132}
+    # lesson_slug.split('-')[-1]
+
+    # TODO: 
+    
+    return render_template("public/lesson.html", lesson=lesson)
 
 @public_blueprint.route('/simulations')
 def simulations():
